e7248192c55910eb14340d6b6f2c6b58c6d73647
ducanh445 <anhndhe170546@fpt.edu.vn>
2026-05-17 00:08:06 +0700

    lab2 ex1

---
+++ lab2/lib/ex1.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  String name = 'Duc Anh';
+  int numberPhone = 0836880711;
+  double height = 1.68;
+  bool isMale = true;
+  
+  print("name: $name");
+  print("numberPhone: $numberPhone");
+  print("height: $height");
+  print("isMale: $isMale");
+}
